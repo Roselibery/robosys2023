35ce992bc7baffd70671433f17eae7f10117bc5d
Rosel <pokerock200@gmail.com>
2023-11-29 12:50:57 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Yuri Sawada
 # SPDX-License-Identifier: BSD-3-Clause
 　
@@ -15,3 +15,17 @@ out=$(seq 5 | ./plus)
 　
 [ "$res" = 0 ] && echo OK        # &&は左側が成功すると右側を実行
 exit $res
+
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+  　
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+  　
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
