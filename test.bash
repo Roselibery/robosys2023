7c59c788b3eeb02c14cf36fe622a51a40e93a6e1
Rosel <pokerock200@gmail.com>
2023-11-30 13:08:17 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -12,17 +12,7 @@ res=0
 out=$(seq 10 | ./plus)
 [ "${out}" = "55
 -55
-3628800" ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo
-exit $res
-
-### I/O ###
-out=$(seq 10 | ./plus)
-[ "${out}" = "55
--55
-3628800" ] || ng ${LINENO}
-
+3628800" ] || ng ${LINENO} "Invalid output"
 
 out=$(echo あ | ./plus)
 [ "$?" = 1 ] || ng ${LINENO}
@@ -31,3 +21,6 @@ out=$(echo あ | ./plus)
 out=$(echo | ./plus) 
 [ "$?" = 1 ] || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
